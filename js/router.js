fb67ddfe249b4530de67bb491d9a2b5a7193ecc3
darad <dabiri.dara@gmail.com>
2014-09-14 16:28:12 +0200

    add route with model function to return all available songs

--- js/router.js
+++ js/router.js
@@ -1,3 +1,10 @@
 Cplist.Router.map(function() {
   this.resource('cplist', { path: '/'});
 });
+
+
+Cplist.CplistRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('song');
+  }
+});
