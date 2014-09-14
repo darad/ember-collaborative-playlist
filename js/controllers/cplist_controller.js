c52140bfccffa9f515d28c7082cc71f30d6a13ac
darad <dabiri.dara@gmail.com>
2014-09-14 17:14:30 +0200

    new controller that handles addition of new songs to the playlist

---
+++ js/controllers/cplist_controller.js
@@ -0,0 +1,26 @@
+Cplist.CplistController = Ember.ArrayController.extend({
+  actions: {
+    addSong:  function() {
+      // Get the song's artist and title by the "New Song" text field
+      var song = this.get('newSong');
+      if (!song) { return false; }
+      if (!song.trim()) { return ; }
+      
+      // parse the artist name and the title of the song
+      var artist = song.split(" - ")[0]
+      var title  = song.split(" - ")[1]
+      
+      // Create the new Song model
+      var song = this.store.createRecord('song', {
+        artist: artist,
+        title:  title
+      });
+      
+      // Clear the "New Song" text field
+      this.set('newSong', '');
+      
+      // Save the new model
+      song.save();
+    }
+  }
+});
